91cebd7adc97103dd156e9adeab204a5758a433a
Yssra Chahine <chahine@live.de>
2026-04-09 17:00:57 +0200

    FEAT: Delete Project

--- components/ProjectDetails.js
+++ components/ProjectDetails.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import BackButton from "./BackButton";
 import MaterialsList from "./MaterialsList";
 import ProjectInfo from "./ProjectInfo";
 import StepsList from "./StepsList";
+import { useRouter } from "next/router";
 
 export default function ProjectDetails({ project }) {
+  const router = useRouter();
+  const [showConfirm, setShowConfirm] = useState(false);
+  async function handleDelete() {
+    const response = await fetch(`/api/projects/${project._id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/");
+    } else {
+      console.error("Delete failed:", response.status);
+    }
+  }
   return (
     <>
       <ProjectInfo
@@ -16,6 +30,18 @@ export default function ProjectDetails({ project }) {
       <MaterialsList materials={project.materials} />
       <StepsList steps={project.steps} />
       <BackButton />
+
+      <button onClick={() => setShowConfirm(true)}>Delete Project</button>
+
+      {showConfirm && (
+        <div>
+          <p>Are you sure you want to delete this project?</p>
+
+          <button onClick={() => setShowConfirm(false)}>Cancel</button>
+
+          <button onClick={handleDelete}>Delete Project</button>
+        </div>
+      )}
     </>
   );
 }
